bac0d3fee1a273241f2f0b4ad84c5ad0150f8cbf
Joseda Rios <josriolop@gmail.com>
2019-06-14 22:19:04 +0200

    Small fixes

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 sudo cp rubbi-sh /usr/local/bin/
 sudo chmod +x /usr/local/bin/rubbi-sh
 
-echo "This tool is based on some aliases and shell functions."
+echo "This tool is based on some alias and shell functions."
 echo ""
 echo "Please import the dotfiles/.rubbi.sh in your shell's configuration file"
 echo "or add its content directly."

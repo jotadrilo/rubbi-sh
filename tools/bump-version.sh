badeefa4e2be9de0ae156f5e6644ccc8efd2266e
Joseda Rios <josriolop@gmail.com>
2019-06-14 13:14:01 +0200

    Rollback version

--- tools/bump-version.sh
+++ tools/bump-version.sh
@@ -6,4 +6,6 @@ major=$(cut -d. -f1 "$version")
 minor=$(cut -d. -f2 "$version")
 patch=$(cut -d. -f3 "$version")
 
-echo "$major.$minor.$((patch+1))" >"$version"
+new_version_string="$major.$minor.$((patch+1))"
+echo "$new_version_string" >"$version"
+git commit -am "Bump VERSION to $new_version_string"

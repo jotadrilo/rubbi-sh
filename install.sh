ab2600e190d661f5be8a5a0a4848e1bf918a8fe0
Joseda Rios <josriolop@gmail.com>
2019-06-14 17:34:50 +0200

    Fix typo

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ sudo chmod +x /usr/local/bin/rubbi-sh
 
 echo "This tool is based on some aliases and shell functions."
 echo ""
-echo "Please import the dotfiles/.aliases.sh in your shell's configuration file"
+echo "Please import the dotfiles/.rubbi.sh in your shell's configuration file"
 echo "or add its content directly."
 echo ""
 echo " . $PWD/dotfiles/.rubbi.sh"
